b4e5b93427bdc04d62f925f18ab6e429493d5870
Srijan Personal <chapssrijan@gmail.com>
2025-02-11 23:00:36 +0200

    deregister quad values plugin temporarily..

--- src/core/tweakPaneConfigurator.ts
+++ src/core/tweakPaneConfigurator.ts
@@ -2,7 +2,7 @@ import { Pane } from "tweakpane";
 import { FolderApi } from "@tweakpane/core";
 import { sanitizeAndExtractCommonCSSProps } from "./commonCSSProps";
 import { flattenPropsFirstAndLastKey } from "./flattenProps";
-import QuadValuesBladePluginBundle from "../../node_modules/plugin-tweakpane-quad-border/src/index";
+//import QuadValuesBladePluginBundle from "../../node_modules/plugin-tweakpane-quad-border/src/index";
 
 // Default four-sided properties handling (e.g., margin, padding)
 export const fourSidedProps = ["margin", "padding", "borderWidth"];
@@ -23,7 +23,7 @@ export class TweakpaneConfig {
 
   constructor(config: PanelConfig) {
     this.pane = new Pane();
-    this.pane.registerPlugin(QuadValuesBladePluginBundle);
+    // this.pane.registerPlugin(QuadValuesBladePluginBundle);
     this.config = config;
     this.config.restructureProps = config.props.restructureProps || true;
     this.props = config.props;
